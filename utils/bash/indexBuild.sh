2730b27a2c654aeb78890b45d6f3ba3c24bb888d
truefibot <truebot.github@trusttoken.com>
2022-06-14 19:41:08 +0000

    🛀 Clean up scripts (#2381)

--- utils/bash/indexBuild.sh
+++ utils/bash/indexBuild.sh
@@ -34,7 +34,7 @@ echo "}" >> $outputDir/index.ts
 
 echo "export * from './types'" >> $outputDir/index.ts
 
-git rev-parse --is-inside-work-tree || exit 0
+git rev-parse --is-inside-work-tree > /dev/null 2>&1 || exit 0
 
 if [[ -z "$(git status --porcelain)" ]]; then
     echo "Build canary stored in ${outputDir}/canary.hash"
