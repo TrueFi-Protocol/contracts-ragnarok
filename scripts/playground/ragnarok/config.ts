5e442c0763eb9793919c42a0b40c9e865885fc6c
truefibot <truebot.github@trusttoken.com>
2022-06-17 21:56:31 +0000

    🌉 Create migration UI for transfer between Alameda SBPs (#2419)

--- scripts/playground/ragnarok/config.ts
+++ scripts/playground/ragnarok/config.ts
@@ -8,7 +8,7 @@ export const config = {
     managerFee: env('MANAGED_PORTFOLIO_MANAGER_FEE', 100), // 1%
     depositMessage: env(
       'MANAGED_PORTFOLIO_DEPOSIT_MESSAGE',
-      'I agree to the terms and conditions',
+      'By signing this transaction you are confirming that you are not a person or entity who resides in, is a citizen of, is located in, is incorporated in, or has a registered office in the United States of America (collectively “US Persons”). If you are a US Person then do not proceed with this transaction, or attempt to use a virtual private network (“VPN”) to circumvent the restrictions set forth herein.',
     ),
   },
   protocol: {
