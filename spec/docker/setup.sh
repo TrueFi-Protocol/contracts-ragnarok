b1e757e616c29d6290f08a37756530a89d8002ca
truefibot <truebot.github@trusttoken.com>
2022-06-30 21:40:36 +0000

    🖼 Refresh FV setup.sh script (#2649)

--- spec/docker/setup.sh
+++ spec/docker/setup.sh
@@ -10,44 +10,22 @@ extract_json() {
 }
 
 extract_version() {
-    local text="$1"
-
-    BASH_REMATCH=""
-    [[ "${text}" =~ [0-9\.]+ ]]
-    echo "${BASH_REMATCH}"
+    RE="/[\d.]*\d/"
+    # prints first match or exits with code 1
+    echo "$1" | tr '\n' ' ' | perl -ne "($RE and print \$&) or exit(1)"
 }
 
 setup_solc() {
     echo "Setting up solc..." >&2
-    local solc_version=""
-    local solc_version="$(extract_version "$(solc --version)" || true)"
     local compiler_version="$(extract_version "$(extract_json .compiler.json version)")"
 
-    echo "Found solc version ${solc_version} and Hardhat compiler version ${compiler_version}." >&2
-    if [[ "${solc_version}" != "${compiler_version}" ]]; then
-        echo "Updating solc version to ${compiler_version}..." >&2
-        pip3 install solc-select
-        solc-select install "${compiler_version}"
-        solc-select use "${compiler_version}"
-    fi
-}
-
-setup_certora() {
-    echo "Setting up Certora..." >&2
-    local certora_version=""
-    local certora_version="$(extract_version "$(certoraRun --version)" || true)"
-    local desired_version="3.1.0"
-
-    echo "Found Certora version ${certora_version} and desired version ${desired_version}." >&2
-    if [[ "${certora_version}" != "${desired_version}" ]]; then
-        echo "Updating Certora version to ${desired_version}..." >&2
-        pip3 install certora-cli==${desired_version}
-    fi
+    echo "Found Hardhat compiler version ${compiler_version}." >&2
+    solc-select install "${compiler_version}"
+    solc-select use "${compiler_version}"
 }
 
 main() {
     setup_solc
-    setup_certora
 }
 
 main "$@"
